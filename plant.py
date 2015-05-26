7b6919c9654a1b482cdc858ba55297dbd6f4b9a6
Javier Domingo Cansino <javierdo1@gmail.com>
2015-05-27 01:31:49 +0200

    Formatting correctly py files and renaming. I have been unable to do anything

--- plant.py
+++ plant.py
@@ -1,45 +1,48 @@
 #!/usr/bin/python2
 from __future__ import print_function
 import sys
+
 sys.path.append('/usr/realtime/rtai-py/')
 import rtai
 import ctypes
 
-class scope_data(ctypes.Structure):
-	_fields_=[('time',ctypes.c_double),
-	('value',ctypes.c_double)]
+
+class scope_data_structure(ctypes.Structure):
+    _fields_ = [
+        ('time', ctypes.c_double),
+        ('control', ctypes.c_double),
+        ('feedback', ctypes.c_double),
+    ]
 
 
-task = rtai.rt_task_init_schmod(rtai.nam2num("LAT1"),20,0,0,0,0XF)
+task = rtai.rt_task_init_schmod(rtai.nam2num("LAT1"), 20, 0, 0, 0, 0XF)
 rtai.rt_make_soft_real_time()
-control_reference = rtai.rt_get_adr(rtai.nam2num("MBX1"))
-plant_output = rtai.rt_get_adr(rtai.nam2num("MBX2"))
+controller_output_mbx = rtai.rt_get_adr(rtai.nam2num("MBX1"))
+plant_feedback_mbx = rtai.rt_get_adr(rtai.nam2num("MBX2"))
 
-salida = scope_data()
-scope_mbx = rtai.rt_mbx_init(rtai.nam2num("MBX3"),ctypes.sizeof(salida))
-print(scope_mbx)
-dato = ctypes.c_double()
-data = ctypes.c_double()
-u1=0
-u2=0
-y1=0
-y2=0
-i=0
+scope_data = scope_data_structure()
+scope_mbx = rtai.rt_mbx_init(rtai.nam2num("MBX3"), ctypes.sizeof(scope_data))
+plant_feedback = ctypes.c_double()
+controller_output = ctypes.c_double()
+u1, u2, y1, y2 = 0, 0, 0, 0
+initial_timestamp = rtai.rt_get_cpu_time_ns()
 
-while(True):
-        i=i+0.005
-	rtai.rt_mbx_receive(control_reference, ctypes.byref(data), ctypes.sizeof(data))
-	u = float(data.value)
-        y = 1.941*y1-0.9418*y2+2.451*0.000001*u1+2.402*0.000001*u2
-	dato.value=y
-	salida.value = y
-	salida.time = i
-	u1=u
-	u2=u1
-	y1=y
-	y2=y1
-	rtai.rt_mbx_send_if(plant_output, ctypes.byref(dato), ctypes.sizeof(dato))
-	rtai.rt_mbx_send_if(scope_mbx, ctypes.byref(salida), ctypes.sizeof(salida))
+while True:
+    rtai.rt_mbx_receive(controller_output_mbx,
+                        ctypes.byref(controller_output),
+                        ctypes.sizeof(controller_output))
+    u = float(controller_output.value)
+    y = 1.941 * y1 - 0.9418 * y2 + 2.451 * 0.000001 * u1 + 2.402 * 0.000001 * u2
+    plant_feedback.value = y
+    scope_data.value = y
+    scope_data.time = rtai.rt_get_cpu_time_ns() - initial_timestamp
+    u1, u2, y1, y2 = u, u1, y, y1
+    rtai.rt_mbx_send_if(plant_feedback_mbx,
+                        ctypes.byref(plant_feedback),
+                        ctypes.sizeof(plant_feedback))
+    rtai.rt_mbx_send_if(scope_mbx,
+                        ctypes.byref(scope_data),
+                        ctypes.sizeof(scope_data))
 
 
 
--- scope.py
+++ scope.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python2
 from __future__ import print_function
 import sys
+
 sys.path.append('/usr/realtime/rtai-py/')
 import rtai
 import ctypes
 import time
-import numpy as np
-import matplotlib.pyplot as plt
+
 
 class scope_data(ctypes.Structure):
-	_fields_=[('time',ctypes.c_double),
-	('value',ctypes.c_double)]
+    _fields_ = [
+        ('time', ctypes.c_double),
+        ('control', ctypes.c_double),
+        ('feedback', ctypes.c_double),
+    ]
 
 
-task = rtai.rt_task_init_schmod(rtai.nam2num("LAT2"),20,0,0,0,0XF)
-plt.axis([0,1000,0,1])
-plt.ion()
+task = rtai.rt_task_init_schmod(rtai.nam2num("LAT2"), 20, 0, 0, 0, 0XF)
 mbx_scope = rtai.rt_get_adr(rtai.nam2num("MBX3"))
-print(mbx_scope)
-scope=scope_data()
+scope = scope_data()
 
-while(True):
-	rtai.rt_mbx_receive(mbx_scope, ctypes.byref(scope), ctypes.sizeof(scope))
-	time = float(scope.time)
-	value = float(scope.value)
-	print(time,value)
-	plt.scatter(time,value)
-	plt.draw()
+while (True):
+    rtai.rt_mbx_receive(mbx_scope, ctypes.byref(scope), ctypes.sizeof(scope))
+    time = float(scope.time)
+    value = float(scope.value)
+    print(time, value)
+    plt.scatter(time, value)
+    plt.draw()
 
  
 
