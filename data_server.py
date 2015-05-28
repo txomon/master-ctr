6079798d6549e66ab91d562bc71475f79b4fc22d
Luis Miguel Ruiz Llarena <luis_mi_50@hotmail.com>
2015-05-28 20:39:58 +0200

    Now it's working, but I need to update the client

---
+++ data_server.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import logging
+import threading
+from collections import deque
+import subprocess
+import time
+
+import cherrypy
+from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
+from ws4py.websocket import WebSocket
+
+try:
+    import simplejson as json
+except ImportError:
+    import json
+
+cherrypy.config.update({'server.socket_port': 9001})
+WebSocketPlugin(cherrypy.engine).subscribe()
+cherrypy.tools.websocket = WebSocketTool()
+
+logging.basicConfig(format=logging.BASIC_FORMAT, level=logging.DEBUG)
+logger = logging.getLogger()
+
+
+class Server(object):
+    def __init__(self):
+        self.connections = []
+        self.scope = subprocess.Popen(['./scope.py'], stdin=subprocess.PIPE, stdout=subprocess.PIPE) 
+        threading.Timer(0,self.send_data, [threading.currentThread()]).start()
+
+    def new_connection(self, con):
+        self.connections.append(con)
+
+    def closed_connection(self, con):
+        self.connections.remove(con)
+
+    def handle_request(self, con, msg):
+        logger.log("Received: " + repr(msg))
+
+    def send_data(self, master_thread):
+        time.sleep(1)
+        while threading.active_count() > 3:
+            data = self.scope.stdout.readline()
+            for con in self.connections:
+                con.send(data)
+        self.scope.kill()
+        self.scope.terminate()
+
+
+app = Server()
+
+
+class WebSocketHandler(WebSocket):
+    def opened(self):
+        app.new_connection(self)
+        print('WS connection opened from', self.peer_address)
+
+    def closed(self, code, reason=None):
+        app.closed_connection(self)
+        print('WS connection closed with code', code, reason)
+
+    def received_message(self, message):
+        try:
+            request = json.loads(message.data)
+        except ValueError as e:
+            logger.exception("JSON not correctly formatted")
+            return
+        except Exception as e:
+            logger.exception('JSON decode failed for message')
+            return
+        app.handle_request(self, request)
+
+    def send(self, payload, binary=False):
+        logger.debug("Answer: " + str(payload))
+        super(WebSocketHandler, self).send(payload, binary)
+
+
+class Root(object):
+    @cherrypy.expose
+    def index(self):
+        handler = cherrypy.request.ws_handler
+
+
+cherrypy.quickstart(Root(), '/', config={
+    '/': {
+        'tools.websocket.on': True,
+        'tools.websocket.handler_cls': WebSocketHandler,
+    },
+})
--- scope.py
+++ scope.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python
 from __future__ import print_function
 import sys
+import select
 
 sys.path.append('/usr/realtime/rtai-py/')
 import rtai
 import ctypes
 import time
-
 import logging
-from threading import Timer
-from collections import deque
-
-import cherrypy
-from ws4py.server.cherrypyserver import WebSocketPlugin, WebSocketTool
-from ws4py.websocket import WebSocket
 
 try:
     import simplejson as json
 except ImportError:
     import json
-
-cherrypy.config.update({'server.socket_port': 9001})
-WebSocketPlugin(cherrypy.engine).subscribe()
-cherrypy.tools.websocket = WebSocketTool()
 
 logging.basicConfig(format=logging.BASIC_FORMAT, level=logging.DEBUG)
 logger = logging.getLogger()
@@ -34,89 +24,71 @@ class scope_data_structure(ctypes.Structure):
         ('feedback', ctypes.c_double),
     ]
 
-task = rtai.rt_task_init_schmod(rtai.nam2num("LAT2"), 20, 0, 0, 0, 0XF)
 
 class RtaiScope(object):
     def __init__(self):
-        self.connections = []
+        task = rtai.rt_task_init_schmod(rtai.nam2num("LAT2"), 20, 0, 0, 0, 0XF)
         self.scope_mbx = rtai.rt_get_adr(rtai.nam2num("MBX3"))
         self.scope = scope_data_structure()
         self.setpoint_mbx = rtai.rt_get_adr(rtai.nam2num("MBX4"))
         self.setpoint = ctypes.c_double()
-	logger.debug("Setting up timer for rtai functionality")
-        t = Timer(2, self.read_rtai)
-        t.start()
-
-    def new_connection(self, con):
-        self.connections.append(con)
-
-    def closed_connection(self, con):
-        self.connections.remove(con)
-
-    def handle_request(self, con, msg):
-        logger.debug("Received: " + repr(msg))
-        if msg['type'] == 'setpoint':
-            self.rtai_setpoint(msg['message'])
 
     def rtai_setpoint(self, setpoint):
         if self.setpoint.value == setpoint:
             return
+        logger.debug('Changing setpoint')
         self.setpoint.value = setpoint
         rtai.rt_mbx_send(self.setpoint_mbx, ctypes.byref(self.setpoint), ctypes.sizeof(self.setpoint))
 
     def read_rtai(self):
-        while True:
-            rtai.rt_mbx_receive(self.scope_mbx, ctypes.byref(self.scope), ctypes.sizeof(self.scope))
-            time = float(self.scope.time)
-            control = float(self.scope.control)
-            feedback = float(self.scope.feedback)
-            data = {
-                "time": time,
-                "control": control,
-                "feedback": feedback,
-            }
-            logger.debug("RTAI reported: "+ repr(data))
-            for con in self.connections:
-                con.send('{"type": "data", "message":' + json.dumps(data) + '}')
+        rtai.rt_mbx_receive(self.scope_mbx, ctypes.byref(self.scope), ctypes.sizeof(self.scope))
+        time = float(self.scope.time)
+        setpoint = float(self.scope.setpoint)
+	self.setpoint.value = setpoint
+        feedback = float(self.scope.feedback)
+        data = {
+            "time": time,
+            "setpoint": setpoint,
+            "feedback": feedback,
+        }
+        print('{"type": "scope", "message":' + json.dumps(data) + '}')
 
-app = RtaiScope()
+class StreamingInput(object):
+    def __init__(self):
+        self.poller = select.poll()
+        self.poller.register(sys.stdin, select.POLLIN)
 
+    def poll(self):
+        poll_res = self.poller.poll(0)
+        if not poll_res:
+            return False
+        logger.debug("Received: "+ repr(poll_res))
+        return poll_res[0][1]
 
-class WebSocketHandler(WebSocket):
-    def opened(self):
-        app.new_connection(self)
-        print('WS connection opened from', self.peer_address)
+    def read(self):
+        poll = self.poll()
+        if poll == 1:
+            return sys.stdin.readline()
+        elif poll > 4:
+            raise ValueError()
+        return None
 
-    def closed(self, code, reason=None):
-        app.closed_connection(self)
-        print('WS connection closed with code', code, reason)
+if __name__ == '__main__':
+    app = RtaiScope()
+    stdin = StreamingInput()
+    while True:
+        app.read_rtai()
+	try:
+            line = stdin.read()
+        except ValueError:
+            sys.exit(0)
+        if not line:
+            continue
 
-    def received_message(self, message):
-        try:
-            request = json.loads(message.data)
-        except ValueError as e:
-            logger.exception("JSON not correctly formatted")
-            return
-        except Exception as e:
-            logger.exception('JSON decode failed for message')
-            return
-        app.handle_request(self, request)
-
-    def send(self, payload, binary=False):
-        logger.debug("Answer: " + str(payload))
-        super(WebSocketHandler, self).send(payload, binary)
-
-
-class Root(object):
-    @cherrypy.expose
-    def index(self):
-        handler = cherrypy.request.ws_handler
-
-
-cherrypy.quickstart(Root(), '/', config={
-    '/': {
-        'tools.websocket.on': True,
-        'tools.websocket.handler_cls': WebSocketHandler,
-    },
-})
-
+        msg = json.dumps(line.strip())
+	if type(msg) in [str, unicode]:
+            logger.debug("Input string: " + repr(msg))
+            msg = {'type': 'setpoint', 'message': float(line)}
+        else:
+            logger.debug('Input json')
+        app.rtai_setpoint(msg['message'])
